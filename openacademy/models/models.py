7a5c884384770dfd26efbcfd827dd7fd8dd54e60
Jose Lee Torres <joseleetorres@gmail.com>
2016-11-19 19:29:23 +0000

    [REF] openacademy: Add relations between models

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -4,8 +4,9 @@
 from odoo import models, fields, api
 
 '''
-This module is to create the Course model
+This module is to create the Course and Session models
 '''
+
 
 class Course(models.Model):
     '''
@@ -14,4 +15,23 @@ class Course(models.Model):
     _name = 'openacademy.course'
 
     name = fields.Char(string="Title", required=True)
-    description = fields.Text(string="Description")
+    description = fields.Text()
+    responsible_id = fields.Many2one('res.users',
+                                     ondelete='set null',
+                                     string="Responsible", index=True)
+    session_ids = fields.One2many('openacademy.session', 'course_id',
+                                  string="Sessions")
+
+
+class Session(models.Model):
+    _name = 'openacademy.session'
+
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6, 2), help="Duration in days")
+    seats = fields.Integer(string="Number of seats")
+    instructor_id = fields.Many2one('res.partner', string="Instructor")
+    course_id = fields.Many2one('openacademy.course',
+                                ondelete='cascade',
+                                string="Course", required=True)
+    attendee_ids = fields.Many2many('res.partner', string="Attendess")
