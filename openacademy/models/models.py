82a0f79dd969815a17b6ec43315787679aff5a3d
Jose Lee Torres <joseleetorres@gmail.com>
2016-11-23 20:38:22 +0000

    [REF] Advanced Views Added Graphics, Kanban

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -56,6 +56,7 @@ class Session(models.Model):
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
     active = fields.Boolean(default=True)
+    color = fields.Integer()
     instructor_id = fields.Many2one('res.partner', string="Instructor",
                                     domain=['|', ('instructor', '=', True),
                                             ('category_id.name', 'ilike',
@@ -67,6 +68,9 @@ class Session(models.Model):
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
     end_date = fields.Date(string="End date", store=True,
         compute='_get_end_date', inverse='_set_end_date')
+    #aqui iria hours para los Gantt Charts, que no los haremos por ahora
+    attendees_count = fields.Integer(
+        string="Attendees count", compute='_get_attendees_count', store=True)    
 
     @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
@@ -116,6 +120,11 @@ class Session(models.Model):
             start_date = fields.Datetime.from_string(r.start_date)
             end_date = fields.Datetime.from_string(r.end_date)
             r.duration = (end_date - start_date).days + 1
+
+    @api.depends('attendee_ids')
+    def _get_attendees_count(self):
+        for r in self:
+            r.attendees_count = len(r.attendee_ids)
 
     @api.constrains('instructor_id', 'attendee_ids')
     def _check_instructor_not_in_attendees(self):
